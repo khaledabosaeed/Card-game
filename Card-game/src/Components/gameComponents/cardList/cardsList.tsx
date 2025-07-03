3b8b9db291959e1b4b6d33c4be47bf3be57bab4d
khaledabosaeed <kahledblata242@gmail.com.com>
2025-07-03 16:12:13 +0300

    feat(card-game): implement card flipping logic and animations
    - Add visible property to ICard interface
    - Implement card flipping animation and state management
    - Update card generator utility to include visible property
    - Enable strict TypeScript compiler options
    - Remove unused BrowserRouter import

--- Card-game/src/Components/gameComponents/cardList/cardsList.tsx
+++ Card-game/src/Components/gameComponents/cardList/cardsList.tsx
@@ -7,12 +7,12 @@ import { cardgenertor } from '../../../utils/game.utils';
 const CurrantLevel = Elevels.Hard;
 
 function CardsList() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [CardsList, SetCardsList] = useState(cardgenertor(CurrantLevel))
     return (
         <div className={`wrapper list-${CurrantLevel}`}>
             {CardsList.map((elm, i) => <Card data={elm} key={i} />)
             }
-
         </div >
     )
 }
